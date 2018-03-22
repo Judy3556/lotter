ebb412eddfa53a402ef2be59bae682ebe44d84ef
m305-14\w401 <1147663235@qq.com>
2018-03-22 16:51:23 +0800

    cool

--- schoolwork322/schoolwork322/Program.cs
+++ schoolwork322/schoolwork322/Program.cs
@@ -37,6 +37,22 @@ namespace schoolwork322
             Console.WriteLine("          0         0 0 0 0          0                ");
             Console.WriteLine("           0                       0                   ");
             Console.WriteLine("             0 0 0 0 0 0 0 0 0 0 0                           ");
+
+            int count = 0;
+            while (count<10)
+            {
+                int a1 = rand.Next(0, 43);
+                int c2 = rand.Next(0, 43);
+                int c3 = rand.Next(0, 43);
+                int c4 = rand.Next(0, 43);
+                int c5 = rand.Next(0, 43);
+                int c6 = rand.Next(0, 43);
+             Console.WriteLine("                 {0:00} {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}", count, a1, c2, c3, c4, c5, c6);
+                count = count + 1;
+            }
+           
+
+
             Console.ReadLine();
         }
     }
