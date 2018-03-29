6eb7729dd5f006f00338f9777f17409a2eaadffa
m305-14\w401 <1147663235@qq.com>
2018-03-29 17:58:34 +0800

    hi

--- work322/work322/Program.cs
+++ work322/work322/Program.cs
@@ -17,21 +17,100 @@ namespace work322
             string[] name = { "葉俊廷", "張澤瑜", "哈哈哈我又来改名字了还是边缘人王程捷", "江儀婷", "張秉華", "陳柏霖", "黃紀瑜", "黃昱維", "蔡逸群", "登琳", "世新魏鈞孝吧", "劉定南", "陳信如", "童信傑", "李岳倫", "鄒和恆", "簡毓玟", "劉子瑄", "蕭紹洋", "李亞宸la", "張以潔", "邱仕紳", "呂家瑩", "世新林鑫佑", "徐均得", "陳佳欣", "張恩瑋", "周詮", "王湘婷", "蕭宇成", "Max Cheung(BANG)", "馬嘉誠Σヽ(ﾟД ﾟ; )ﾉ魔貫光殺砲(ﾟДﾟ)σ━0000", "陳昱嘉", "康珈熏", "盧奕宏", "陳郁佳的大哥黃博涵", "遲正雯", "世新林星彤", "許晏誠ｘ勒是奇多＿８＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝Ｄ　", "許子安", "高子晴", "陳思婷", "矝鵘鮿", "余彥廷", "吳耀輝", "鄭丞智", "林冠廷", "世新何曜宇", "邵喬雨", "張豐愷", "林子晴", "邱雁回", "陳家欣是韓妞", "鄧雅馨", "謝汶珊", "李曼寧", "世新徐偉哲", "张钰慈", "鍾宜珊", "raer_tsai", "世新李姳諼", "鄭曼君", "世新陳柏霖", "世新許子安", "世新方若帆" };
             int[] heigh = { 173, 0, 179, 155, 183, 170, 163, 174, 165, 189, 177, 180, 154, 170, 170, 173, 165, 158, 180, 165, 153, 175, 162, 165, 0, 160, 165, 173, 164, 177, 177, 180, 170, 151, 176, 180, 168, 152, 666, 2147483647, 165, 155, 777, 173, 169, 170, 169, 171, 0, 170, 150, 160, 155, 164, 163, 165, 184, 0, 155, 0, 168, 160, 169, 0, 150 };
             Random rand = new Random();
-            Console.WriteLine();
+             Console.WriteLine();
             int herN = -1;
             string sn = Console.ReadLine();
             int r = rand.Next(0, heigh.Length);
             Console.WriteLine(namber[r]+" "+blood[r]+" "+fm[r]+" "+star[r]+" "+name[r]+" " + r + ": " + heigh[r]);
             for (int i=0; i < name.Length; i++)
             {
-                if (sn == namber[i])
+            if (sn == namber[i])
+            {
+              herN = i;
+
+             }
+             int count = 0;
+            int A = 0;
+            int B = 0;
+            int C = 0;
+             for(int j = 0; j < blood.Length; j++)
+            {
+                Console.WriteLine(blood[j]);
+                if(blood[j] == "O")
                 {
-                    herN = i;
+                       count ++;
 
                 }
-            }
-            Console.WriteLine(herN);
-            Console.ReadLine();
+                if(blood[j]=="A")
+                {
+                      A++;
                 }
+                if (blood[j] == "B")
+                {
+                      B++;
+                    }
+                    if (blood[j] == "AB")
+                    {
+                      C++;
+                }
+                
+
+            }
+            double o = (double)count / blood.Length * 100;
+            double a = (double)A/ blood.Length*100;
+            double b = (double)B / blood.Length * 100;
+            double ab = (double)C / blood.Length * 100;
+            Console.WriteLine("o="+count+"        a="+A+"        b:"+B+"        AB="+C);
+            Console.WriteLine( o + " %" + a + "% " + b + "% " + ab + "% ");
+             int h=0;
+             int w = 0;
+            int sum = 0;
+             int sam = 0;
+             for(int i = 0; i < fm.Length; i++)
+            {
+                 if (fm[i] == "女")
+                {
+                    //  h++;
+                    //  sum = sum + heigh[i];
+
+                }
+                //if (fm[i] == "男")
+                {
+                    //  w++;
+                    //  sam = sam + heigh[i];
+
+                }
+
+            }
+
+            // double bm = (double)sam / w;
+            //double gm = (double)sum / h;
+            //Console.WriteLine("   "+sum+"  "+gm+"   "+sam+"  "+bm);
+            int max = 0;
+            int min = 0;
+            for (int i = 0; i < heigh.Length; i++)
+            {
+                if (heigh[i] > 100 && heigh[i] < 200)
+                {
+                    if (heigh[i] > max)
+                    {
+                        max = heigh[i];
+                    }
+                }
+                else if (heigh[i] > 100 && heigh[i] < 200)
+                {
+                    if (heigh[i] < min)
+                    {
+                        min = heigh[i];
+                    }
+                    Console.WriteLine(min + " " + max);
+
+                    Console.Read();
+                }
+                //Console.WriteLine(herN);
+            }
+        }
     }
 }
+
+    
