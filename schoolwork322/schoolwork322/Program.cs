0a237e12227d72d0dc8e9941e671e04c8881a006
m305-14\w401 <1147663235@qq.com>
2018-04-26 18:10:07 +0800

    增加了注释

--- schoolwork322/schoolwork322/Program.cs
+++ schoolwork322/schoolwork322/Program.cs
@@ -10,14 +10,19 @@ namespace schoolwork322
     {
         static void Main(string[] args)
         {
+            //創建一個新的隨機亂數生成器
             Random rand = new Random();
 
+            //在0到43之間產生6個亂數作為中獎號碼
             int r1 = rand.Next(0, 43);
             int r2 = rand.Next(0, 43);
             int r3 = rand.Next(0, 43);
             int r4 = rand.Next(0, 43);
             int r5 = rand.Next(0, 43);
             int r6 = rand.Next(0, 43);
+
+            //列出所產生的中獎號碼
+            Console.WriteLine("                    超級大樂透");
             Console.WriteLine();
             Console.WriteLine("                   本期中獎號碼為");
             Console.WriteLine("                -------------------");
@@ -38,8 +43,11 @@ namespace schoolwork322
             Console.WriteLine("           0                       0                   ");
             Console.WriteLine("             0 0 0 0 0 0 0 0 0 0 0                           ");
 
+            //定義一個計算數量的數
             int count = 0;
-            while (count<10)
+
+            //定義該程式隨機在0到43範圍內生成6個數且該程式只能運行10次
+            while (count < 10)
             {
                 int a1 = rand.Next(0, 43);
                 int c2 = rand.Next(0, 43);
@@ -47,12 +55,11 @@ namespace schoolwork322
                 int c4 = rand.Next(0, 43);
                 int c5 = rand.Next(0, 43);
                 int c6 = rand.Next(0, 43);
-             Console.WriteLine("                 {0:00} {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}", count, a1, c2, c3, c4, c5, c6);
+                Console.WriteLine("              {0:00} {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}", count, a1, c2, c3, c4, c5, c6);
                 count = count + 1;
             }
-           
 
-
+            //觀察結果
             Console.ReadLine();
         }
     }
